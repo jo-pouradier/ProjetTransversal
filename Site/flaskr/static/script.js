3c12f1fe96d526a2f47313b6778d95e56f2c5dc1
Maxime Giroud <maxime.giroud@cpe.fr>
2023-04-18 16:56:59 +0200

    Ajout clavier

--- Site/flaskr/static/script.js
+++ Site/flaskr/static/script.js
@@ -5,3 +5,13 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  document.addEventListener('keydown', function(event) {
+    // Vérifie si la touche appuyée est la touche "a"
+    if (event.key === 'a') {
+      // Envoie une requête POST à l'URL '/appeler_fonction'
+      fetch('/appeler_fonction', {
+        method: 'POST'
+      });
+    }
+  });
