bb02ecc595eb09d5808d49dcfe2b9372db414945
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-04-25 17:28:23 +0200

    k

--- Site/testCommandeSerial.py
+++ Site/testCommandeSerial.py
@@ -2,6 +2,6 @@ import serial
 
 ser = serial.Serial('/dev/ttyUSB0')#change this to the name of your port
 ser.baudrate = 115200 
-ser.write(b"avancerR\r")
+ser.write("avancerR".encode())
 
 print("avancerR")
