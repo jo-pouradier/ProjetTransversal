1c6105f0edbdb61b2d9e7318f7690b5daa80ca9a
ShezanM <shezan.mahamodraza@cpe.fr>
2023-04-25 14:05:39 +0200

    ajout de la fonction obstacle dans le main

--- Site/flaskr/main.py
+++ Site/flaskr/main.py
@@ -3,8 +3,8 @@ import cv2,time
 import numpy as np
 import pyaudio
 import keyboard 
+import obstacle as obs
 #from scipy.signal import butter, lfilter
-
 app = Flask(__name__)
 
 camera= cv2.VideoCapture(0)
@@ -23,6 +23,7 @@ for i in range(audio1.get_device_count()):
     info = audio1.get_device_info_by_index(i)
     if info['maxInputChannels'] > 0:
         print(f"Index : {i}, Nom : {info['name']}")
+
 
 def liveCam() :
     '''Live camera feed
