18bb430de1709422595b79232b7b875966918230
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-05-04 12:09:46 +0200

    js

--- Site/flaskr/static/script.js
+++ Site/flaskr/static/script.js
@@ -6,9 +6,6 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-// let lastJson = {};
-// let jsonData = event.key;
-// if (JSON.stringify(jsonData) !== '{}' || JSON.stringify(lastJSON) !== '{}'){
 document.addEventListener('keydown', function(event) {
   // Envoie une requête POST à l'URL '/appeler_fonction'
   // on peut envoyer plusieurs fois la meme commande 
@@ -18,5 +15,4 @@ document.addEventListener('keydown', function(event) {
   xhr.setRequestHeader('Content-Type','application/json');
   var send_data = {key:event.key};
   xhr.send(JSON.stringify(send_data));
-  lastJson = jsonData;
 });
