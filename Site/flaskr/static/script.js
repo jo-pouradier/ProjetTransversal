a7bb43c271dc77d60b4ab4e8659b745c012e17bb
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-05-04 12:06:26 +0200

    post

--- Site/flaskr/static/script.js
+++ Site/flaskr/static/script.js
@@ -13,7 +13,7 @@ document.addEventListener('keydown', function(event) {
   // Envoie une requête POST à l'URL '/appeler_fonction'
   // on peut envoyer plusieurs fois la meme commande 
   var xhr = new XMLHttpRequest();
-  var url = '/deplacements';
+  var url = '/commandes';
   xhr.open("POST",url,true);
   xhr.setRequestHeader('Content-Type','application/json');
   var send_data = {key:event.key};
