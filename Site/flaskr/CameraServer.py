d8202fc53e5c95f05f3c209192d6e7ce7ccb00e3
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-05-09 16:01:24 +0200

    ok

--- Site/flaskr/CameraServer.py
+++ Site/flaskr/CameraServer.py
@@ -11,7 +11,7 @@ class CameraServer() :
             "compteur" : 0,
             "anglex" : 0,
             "angley" : 0,
-            "FPS" : 3,
+            "FPS" : 60,
             "WIDTH" : 5,
             "HEIGHT" : 5,
             "faceCascade" : cv2.CascadeClassifier(self.front_face_path)
@@ -101,4 +101,4 @@ class CameraServer() :
 
                 
     def run(self) :
-        self.Detection()
+        self.liveCam()
