a94f303ecd55f1f585a520cd4823c5c56bf552e8
ShezanM <shezan.mahamodraza@cpe.fr>
2023-05-23 17:24:01 +0200

    testson

--- Site/flaskr/main3.py
+++ Site/flaskr/main3.py
@@ -9,6 +9,7 @@ import requests
 import time
 import pyaudio
 
+
 #ATTENTION : VERIFIER PORT + BAUD RATE
 #ser = serial.Serial('/dev/ttyACM0')#change this to the name of your port
 #ser.flushInput()
@@ -160,7 +161,7 @@ def detection() :
 
 
             #Printing the number of face found
-            print ("Found {0} faces!".format(len(faces)))
+            # print ("Found {0} faces!".format(len(faces)))
 
             cam_config["compteur"]+=1
             ret,buffer=cv2.imencode('.jpg',frame)
@@ -171,19 +172,45 @@ def detection() :
                 break
 
 
+def genHeader(sampleRate, bitsPerSample, channels):
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
+
 def rec_sound():
+        p = pyaudio.PyAudio()
+        print('first sound')
         CHUNK_SIZE = 1024
         FORMAT = pyaudio.paInt16
         ## Adaptez ces paramètres 
-        CHANNELS = 1
+        CHANNELS = 2
         RATE = 44100
         
-        p = pyaudio.PyAudio()
+        # p = pyaudio.PyAudio()
+        print("start")
+        bitsPerSample = 16
+        channels = 2
+        wav_header = genHeader(RATE, bitsPerSample, channels)
         stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+        data = wav_header + stream.read(CHUNK_SIZE)
         while True:
-            data = stream.read(CHUNK_SIZE)
-            yield(data)
+               data += stream.read(CHUNK_SIZE)
+               yield(data)
+               
 
+        
 
 # --------------------------------------------------------------------------------------------
 # Routes
@@ -231,23 +258,10 @@ def protected_route():
     return  "Vous êtes connecté en tant que : {} et votre adresse IP est autorisée.".format(auth.current_user())
 
 
-def rec_sound():
-    CHUNK_SIZE = 1024
-    FORMAT = pyaudio.paInt16
-    ## Adaptez ces paramètres 
-    CHANNELS = 1
-    RATE = 44100
-        
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
-    while True:
-        data = stream.read(CHUNK_SIZE)
-        yield(data)
-
-
-@app.route('/audio_stream')
+@app.route("/audio_stream")
 def audio_stream():
-    return flask.Response(rec_sound(),mimetype='audio/x-wav')
+    print("coucou")
+    return flask.Response(rec_sound(),mimetype='audio/x-wav; codec=pcm')
 
 def main():
     app.run(host='0.0.0.0', port=5001)
